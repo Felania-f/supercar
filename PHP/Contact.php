40b3e2cbe5bfa13e25975a239119691c08b83613
felania-f <felania71@gmail.com>
2023-11-12 21:41:50 +0400

    #main

--- PHP/Contact.php
+++ PHP/Contact.php
@@ -15,49 +15,11 @@
         integrity="sha512-9X3q2Y1+D/7VkcE+mRjL7Jz2cTfjJbR8Gx9XVGvY04ER0ZJjLs8Wwq0sD4yKjDh1i4/aW0myX29vHkOiy/oZLQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!--using-fontAwesome------------>
+    <?php include 'navbar.php'; ?>
 </head>
 
 <body>
-
-    <nav>
-        <div class="logo">
-            <a href="../PHP/Accueil.php">
-                <img src="../Image/MicrosoftTeams-image.png" alt="Your Logo">
-            </a>
-        </div>
-        <ul class="menu">
-            <li><a href="../PHP/Accueil.php">Accueil</a></li>
-            <li><a href="../PHP/Voiture.php">Voitures</a></li>
-            <li><a href="../PHP/Demande_essai.php">Demande d'essai</a></li>
-            <li><a href="../PHP/evenement.php">Evenements</a></li>
-            <li><a href="../PHP/Contact.php">Contact</a></li>
-        </ul>
-
-        <?php
-        session_start();
-
-        if (isset($_SESSION['nom']) && isset($_SESSION['prenom'])) {
-            $nom = $_SESSION['nom'];
-            $prenom = $_SESSION['prenom'];
-            echo "<div  class='dropdown'>
-                              <a>$nom $prenom</a>
-                              <div class='dropdown-content'>
-                              <a href='profile.php'>Profil</a>
-                            <a href='deconnexion.php'>Déconnexion</a>
-                            </div>
-                            </div>";
-        } else {
-            echo "<div class='login'>
-                            <a href='inscription.php'>Connexion</a>
-                            </div>";
-        }
-
-        ?>
-
-    </nav>
-
     <?php
-
     echo '<div id="grad1">';
     echo '<img src="../Image/desktop-wallpaper-bmw-m3-angel-eyes-black-background-black-dark-bmw-m3-iphone.jpg" height="756px" width="310px">';
     echo '<!--contact-form-container------------------->';
@@ -94,9 +56,7 @@
     echo '<img align="right" src="../Image/image.jpg" height="756px" width="310px">';
     echo '</div>';
     echo '</div>';
-
     ?>
-
 
     <?php
     // Connexion à la base de données MySQL
@@ -136,46 +96,22 @@
     ?>
 
     <div class="footer-basic">
-
         <footer>
-
             <div class="line">
-
                 <ul class="social_icon">
-
-
-
                     <li><a href="https://www.facebook.com/"><ion-icon name="logo-facebook"></ion-icon></a></li>
-
                     <li><a href="https://www.twitter.com"><ion-icon name="logo-twitter"></ion-icon></a></li>
-
                     <li><a href="#"><ion-icon name="logo-linkedin"></ion-icon></a></li>
-
                     <li><a href="https://www.instagram.com/"><ion-icon name="logo-instagram"></ion-icon></a></li>
-
                 </ul>
-
                 <UL class="menus">
-
                     <li><a href="Privacy.html">Politique de Confidentialité</a></li>
-
                     <li><a href="mentionlegale.html">Mention légale</a></li>
-
                 </UL>
-
                 <p> ©2023 SuperCar | Le meilleur pour vous</p>
-
         </footer>
-
         <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-
         <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-
-
-
     </div>
-
-
 </body>
-
 </html>
